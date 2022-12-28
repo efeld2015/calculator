06315fb7fb10feabcba00830353d87819bb8f6cb
efeld2015 <efeld@Emily-Felds-MacBook-Pro.local>
2022-12-28 15:48:18 -0600

    added button hover effects, backend logic

--- script.js
+++ script.js
@@ -1,57 +1,37 @@
 // numbers 1 -9, multiplication, division, sub, add, squared, and equals
 // querySelector corresponds to id and classes in html
-const numberZero = document.querySelector('#zero');
-const numberOne = document.querySelector('#one');
-const numberTwo = document.querySelector('#two');
-const numberThree = document.querySelector('#three');
-const numberFour = document.querySelector('#four');
-const numberFive = document.querySelector('#five');
-const numberSix = document.querySelector('#six');
-const numberSeven = document.querySelector('#seven');
-const numberEight = document.querySelector('#eight');
-const numberNine = document.querySelector('#nine');
-const symbolMultiply = document.querySelector('#multiply');
-const symbolDivision = document.querySelector('#divide');
-const symbolPlus = document.querySelector('#plus');
-const symbolMinus = document.querySelector('#minus');
-const symbolDecimalPoint = document.querySelector('#decimal');
-const symbolEquals = document.querySelector('#equals');
-const clearButton = document.querySelector('#clear');
+const calculatorButtons = document.querySelector('#calculator-buttons');
 
 //^ vars go on top
 const inputDisplay = document.querySelector('#inputDisplay');
 
 // global scoped variables to store numbers and operators
-let numbers = [];
-let operators = [];
 let solution = 0;
 let currentNumber = '';
+let lastNumber = '';
+let operator = '';
 let display = '';
-
-// arithmetic functions
-function multiply(param1, param2) {
-  // multiply param1 and param2
-  return param1 * param2;
-}
-
-function division(param1, param2) {
-  return param1 / param2;
-}
-
-function plus(param1, param2) {
-  return param1 + param2;
-}
 
 function clear() {
   display = '';
-  numbers = [];
-  operators = [];
   currentNumber = '';
+  lastNumber = '';
+  operator = '';
+  solution = 0;
   updateDisplay();
 }
 
-function setDisplay(text) {
+function setDisplay(text, override) {
+  if (override) {
+  
+  display = toFixedIfNecessary(text, 4);
+  return;
+  }
   display += text;
+}
+
+function toFixedIfNecessary( value, decimal ){
+  return +parseFloat(value).toFixed( decimal );
 }
 
 function updateDisplay() {
@@ -62,9 +42,36 @@ function updateDisplay() {
   }
 }
 
-// Click functions
+function solve(operator, first, second) {
+  // create variable to store solution value
+  let value;
+  switch(operator) {
+    case 'X': value = Number(first) * Number(second);
+    break;
 
+    case '-': value = Number(first) - Number(second);
+    break;
+
+    case '+': value = Number(first) + Number(second);
+    break;
+
+    case '/': value = Number(first) / Number(second);
+    break;  
+  }
+  return value;
+}
+
+// Click functions
 function handleClick(event) {
+  if (event.target.localName !== 'button') {
+    return;
+  }
+
+  // if solution exists, reset
+  if (solution !== 0) {
+    clear()
+  }
+
   // set innerText to a variable
   const innerText = event.target.innerText;
 
@@ -78,60 +85,29 @@ function handleClick(event) {
   updateDisplay();
 
   // LOGIC
-  if ( innerText === 'X' || innerText === '-' || innerText === '+' || innerText === '/' ) {
-  // insert number value and operator value into arrays
-  numbers.push(currentNumber);
-  operators.push(innerText);
-  currentNumber = ''; 
-  console.log('number array', numbers, 'operator array', operators)
 
-  // TODO: clear display
-  // display = numbers[0];
-  // updateDisplay();
+  // An array of different operator options
+  let operatorOptions = ['X', '-', '+', '/'];
 
+  // if innerText is an operator
+  if (operatorOptions.includes(innerText)) {
+    lastNumber = currentNumber;
+    operator = innerText;
+    currentNumber = ''
+    
   }
   else if (innerText === '=') {
-    for (let index = 0; index < numbers.length - 1; index++) {
-      if (index === 0) {
-        solution = numbers[0]
-      } else {
-        let thisNumber = numbers[index];
-        let thisOperator = operators[index - 1]
-        if (thisOperator === 'X') {
-          solution = solution * thisNumber
-        } else if (thisOperator === '-') {
-          solution = solution = thisNumber
-        } else if (thisOperator === '+') {
-          solution = solution + thisNumber
-        } else if (thisOperator === '/') {
-          solution = solution / thisNumber
-        }
-      }
+    if (operator) {
+      solution = solve(operator, lastNumber, currentNumber);
+      setDisplay(solution.toString(), true);
+      updateDisplay()
     }
-    console.log(solution)
   } else {
     // sets currentNumber global variable equal to it's original value PLUS the value of innerText
     currentNumber = currentNumber + innerText;
-    console.log(currentNumber)
-  }
+}
 }
 
 // event listeners
 // event listeners are listening/waiting for you to click a button. Something's gonna happen!
-numberZero.addEventListener('click', handleClick);
-numberOne.addEventListener('click', handleClick);
-numberTwo.addEventListener('click', handleClick);
-numberThree.addEventListener('click', handleClick);
-numberFour.addEventListener('click', handleClick);
-numberFive.addEventListener('click', handleClick);
-numberSix.addEventListener('click', handleClick);
-numberSeven.addEventListener('click', handleClick);
-numberEight.addEventListener('click', handleClick);
-numberNine.addEventListener('click', handleClick);
-symbolMultiply.addEventListener('click', handleClick);
-symbolPlus.addEventListener('click', handleClick);
-symbolMinus.addEventListener('click', handleClick);
-symbolDivision.addEventListener('click', handleClick);
-symbolDecimalPoint.addEventListener('click', handleClick);
-symbolEquals.addEventListener('click', handleClick);
-clearButton.addEventListener('click', handleClick);
+calculatorButtons.addEventListener('click', handleClick);
